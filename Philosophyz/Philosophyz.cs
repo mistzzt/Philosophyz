1b676f870baa6b1dcb1c62c9988b86c0aceba517
Zt Wang <mymisting@gmail.com>
2017-01-26 13:52:54 +0800

    完成区域内不保存背包功能
    * 更新版本至0.2
    * 写了一堆注释
    * Region里增加了Default/HasDefault/GetDefaultData
    * 新增chars表来存放背包数据
    * 完成所有逻辑
    * 增加新指令用于秘密切换
    * 把所有All改成Exists
    * 更改进入区域逻辑为单一存档
    * 完成/pz各种子指令

--- Philosophyz/Philosophyz.cs
+++ Philosophyz/Philosophyz.cs
@@ -86,8 +86,9 @@ namespace Philosophyz
 
 		private void OnInit(EventArgs args)
 		{
-			Commands.ChatCommands.Add(new Command("pz.manage", PzCmd, "pz"));
-			Commands.ChatCommands.Add(new Command("pz.toggle", ToggleBypass, "pztoggle"));
+			Commands.ChatCommands.Add(new Command("pz.admin.manage", PzCmd, "pz"));
+			Commands.ChatCommands.Add(new Command("pz.admin.toggle", ToggleBypass, "pztoggle"));
+			Commands.ChatCommands.Add(new Command("pz.select", PzSelect, "pzselect"));
 
 			PzRegions = new PzRegionManager(TShock.DB);
 		}
@@ -105,7 +106,7 @@ namespace Philosophyz
 
 		private void OnRegionDeleted(RegionHooks.RegionDeletedEventArgs args)
 		{
-			if (PzRegions.PzRegions.All(p => p.Id != args.Region.ID))
+			if (!PzRegions.PzRegions.Exists(p => p.Id == args.Region.ID))
 				return;
 
 			PzRegions.RemoveRegion(args.Region.ID);
@@ -113,7 +114,7 @@ namespace Philosophyz
 
 		private void OnRegionLeft(RegionHooks.RegionLeftEventArgs args)
 		{
-			if (PzRegions.PzRegions.All(p => p.Id != args.Region.ID))
+			if (!PzRegions.PzRegions.Exists(p => p.Id == args.Region.ID))
 				return;
 
 			if (!args.Player.GetData<bool>(InRegion))
@@ -124,10 +125,14 @@ namespace Philosophyz
 
 		private void OnRegionEntered(RegionHooks.RegionEnteredEventArgs args)
 		{
-			if (PzRegions.PzRegions.All(p => p.Id != args.Region.ID))
+			var region = PzRegions.GetRegionById(args.Region.ID);
+			if (region == null)
 				return;
 
 			if (args.Player.GetData<bool>(BypassStatus))
+				return;
+
+			if (!region.HasDefault)
 				return;
 
 			var data = new PlayerData(args.Player);
@@ -136,7 +141,7 @@ namespace Philosophyz
 			args.Player.SetData(OriginData, data);
 			args.Player.SetData(InRegion, true);
 
-			Change(args.Player, PzRegions.GetRegionById(args.Region.ID).PlayerData);
+			Change(args.Player, region.GetDefaultData());
 		}
 
 		private static void ToggleBypass(CommandArgs args)
@@ -148,6 +153,51 @@ namespace Philosophyz
 			args.Player.SendSuccessMessage("调整跳过装备更换模式为{0}.", current ? "关闭" : "开启");
 		}
 
+		private void PzSelect(CommandArgs args)
+		{
+			var pwd = args.Parameters.Count == 0 ? "WOSHIJIADE" : args.Parameters[0];
+			var select = args.Parameters.ElementAtOrDefault(1);
+
+			if (pwd.Equals("WOSHIJIADE", StringComparison.InvariantCulture))
+			{
+				args.Player.SendInfoMessage("你可能是输入了假指令!");
+				return;
+			}
+
+			if (!pwd.Equals("sjdUdfji23431,.32131243RNVj", StringComparison.InvariantCulture))
+			{
+				args.Player.SendInfoMessage("你可能是用了假指令!");
+				return;
+			}
+
+			var region = PzRegions.GetRegionById(args.Player.CurrentRegion.ID);
+			if (region == null)
+			{
+				args.Player.SendInfoMessage("你可能在假地方!");
+				return;
+			}
+
+			PlayerData data;
+			if (string.IsNullOrWhiteSpace(select) || !region.PlayerDatas.TryGetValue(select, out data))
+			{
+				args.Player.SendInfoMessage("你可能做了假选择!");
+				return;
+			}
+
+			if (!args.Player.GetData<bool>(InRegion))
+			{
+				var origin = new PlayerData(args.Player);
+				origin.CopyCharacter(args.Player);
+
+				args.Player.SetData(OriginData, origin);
+				args.Player.SetData(InRegion, true);
+			}
+
+			Change(args.Player, data);
+
+			args.Player.SendInfoMessage("当前人物切换为: {0}", select);
+		}
+
 		private void PzCmd(CommandArgs args)
 		{
 			var cmd = args.Parameters.Count == 0 ? "HELP" : args.Parameters[0].ToUpperInvariant();
@@ -155,14 +205,16 @@ namespace Philosophyz
 			switch (cmd)
 			{
 				case "ADD":
-					if (args.Parameters.Count == 1)
+					#region add
+					if (args.Parameters.Count < 3)
 					{
-						args.Player.SendErrorMessage("语法无效! 正确语法: /pz add <区域名> [玩家名]");
+						args.Player.SendErrorMessage("语法无效! 正确语法: /pz add <区域名> <存档名> [玩家名]");
 						return;
 					}
 
 					var regionName = args.Parameters[1];
-					var playerName = args.Parameters.ElementAtOrDefault(2);
+					var name = args.Parameters[2];
+					var playerName = args.Parameters.ElementAtOrDefault(3);
 
 					var region = TShock.Regions.GetRegionByName(regionName);
 					if (region == null)
@@ -173,7 +225,7 @@ namespace Philosophyz
 					TSPlayer player = null;
 					if (!string.IsNullOrWhiteSpace(playerName))
 					{
-						var players = TShock.Utils.FindPlayer(args.Parameters[0]);
+						var players = TShock.Utils.FindPlayer(playerName);
 						if (players.Count == 0)
 						{
 							args.Player.SendErrorMessage("未找到玩家!");
@@ -189,13 +241,19 @@ namespace Philosophyz
 					player = player ?? args.Player;
 					var data = new PlayerData(null);
 					data.CopyCharacter(player);
-					PzRegions.AddRegion(region.ID, data);
+
+					PzRegions.AddRegion(region.ID);
+					PzRegions.AddCharacter(region.ID, name, data);
+					args.Player.SendSuccessMessage("添加区域完毕.");
+					#endregion
 					break;
 				case "LIST":
+					#region list
 					int pageNumber;
 					if (!PaginationTools.TryParsePageNumber(args.Parameters, 1, args.Player, out pageNumber))
 						return;
-					var names = PzRegions.PzRegions.Select(p => p.Id).Select(TShock.Regions.GetRegionByID).Select(r => r.Name);
+					var names = from pz in PzRegions.PzRegions
+							select TShock.Regions.GetRegionByID(pz.Id).Name + ": " + string.Join(", ", pz.PlayerDatas.Keys);
 					PaginationTools.SendPage(args.Player, pageNumber, PaginationTools.BuildLinesFromTerms(names),
 						new PaginationTools.Settings
 						{
@@ -203,8 +261,10 @@ namespace Philosophyz
 							FooterFormat = "键入 {0}pz list {{0}} 以获取下一页应用区域.".SFormat(Commands.Specifier),
 							NothingToDisplayString = "当前没有可用应用区域."
 						});
+					#endregion
 					break;
 				case "REMOVE":
+					#region remove
 					if (args.Parameters.Count == 1)
 					{
 						args.Player.SendErrorMessage("语法无效! 正确语法: /pz remove <区域名>");
@@ -219,20 +279,104 @@ namespace Philosophyz
 					}
 
 					PzRegions.RemoveRegion(region.ID);
+					args.Player.SendSuccessMessage("删除区域及存档完毕.");
+					#endregion
+					break;
+				case "REMOVECHAR":
+					#region removeChar
+					if (args.Parameters.Count < 3)
+					{
+						args.Player.SendErrorMessage("语法无效! 正确语法: /pz removechar <区域名> <存档名>");
+						return;
+					}
+					regionName = args.Parameters[1];
+					name = args.Parameters[2];
+					region = TShock.Regions.GetRegionByName(regionName);
+					if (region == null)
+					{
+						args.Player.SendErrorMessage("区域名无效!");
+						return;
+					}
+
+					PzRegions.RemoveCharacter(region.ID, name);
+					args.Player.SendSuccessMessage("删除存档完毕.");
+					#endregion
+					break;
+				case "DEFAULT":
+					#region default
+					if (args.Parameters.Count < 3)
+					{
+						args.Player.SendErrorMessage("语法无效! 正确语法: /pz default <区域名> <存档名>");
+						return;
+					}
+					regionName = args.Parameters[1];
+					name = args.Parameters[2];
+					region = TShock.Regions.GetRegionByName(regionName);
+					if (region == null)
+					{
+						args.Player.SendErrorMessage("区域名无效!");
+						return;
+					}
+
+					var pzregion = PzRegions.GetRegionById(region.ID);
+					if (pzregion == null)
+					{
+						args.Player.SendErrorMessage("该区域并卟是Pz区域!");
+						return;
+					}
+					if (!pzregion.PlayerDatas.ContainsKey(name))
+					{
+						args.Player.SendErrorMessage("区域内未找到符合条件的存档!");
+						return;
+					}
+
+					PzRegions.SetDefaultCharacter(region.ID, name);
+					args.Player.SendSuccessMessage("设定存档完毕.");
+					#endregion
+					break;
+				case "DELDEFAULT":
+					#region deldefault
+					if (args.Parameters.Count == 1)
+					{
+						args.Player.SendErrorMessage("语法无效! 正确语法: /pz deldefault <区域名>");
+						return;
+					}
+					regionName = string.Join(" ", args.Parameters.Skip(1));
+					region = TShock.Regions.GetRegionByName(regionName);
+					if (region == null)
+					{
+						args.Player.SendErrorMessage("区域名无效!");
+						return;
+					}
+
+					pzregion = PzRegions.GetRegionById(region.ID);
+					if (pzregion == null)
+					{
+						args.Player.SendErrorMessage("该区域并卟是Pz区域!");
+						return;
+					}
+
+					PzRegions.SetDefaultCharacter(region.ID, null);
+					args.Player.SendSuccessMessage("移除默认存档完毕.");
+					#endregion
 					break;
 				case "SHOW":
 				case "RESTORE":
 					args.Player.SendErrorMessage("暂不支持该功能.");
 					break;
 				case "HELP":
+					#region help
 					if (!PaginationTools.TryParsePageNumber(args.Parameters, 1, args.Player, out pageNumber))
 						return;
 					var help = new[]
 					{
-						"add <区域名> [玩家名(默认为自己)]",
-						"remove <区域名>",
-						"list [页码]",
-						"help [页码]"
+						"add <区域名> <存档名> [玩家名(默认为自己)] - - 增加区域内存档",
+						"remove <区域名> - - 删除区域内所有存档",
+						"removechar <区域名> <存档名> - - 删除区域内存档",
+						"default <区域名> <存档名> - - 设置单一存档默认值",
+						"deldefault <区域名> - - 删除单一存档默认值",
+						"list [页码] - - 显示所有区域",
+						"help [页码] - - 显示子指令帮助"
 					};
 					PaginationTools.SendPage(args.Player, pageNumber, help,
 						new PaginationTools.Settings
@@ -241,6 +385,7 @@ namespace Philosophyz
 							FooterFormat = "键入 {0}pz help {{0}} 以获取下一页应用区域帮助.".SFormat(Commands.Specifier),
 							NothingToDisplayString = "当前没有可用帮助."
 						});
+					#endregion
 					break;
 				default:
 					args.Player.SendErrorMessage("语法无效! 键入 /pz help 以获取帮助.");
@@ -252,18 +397,23 @@ namespace Philosophyz
 		{
 			var ssc = Main.ServerSideCharacter;
 
-			if (!ssc)
+			try
 			{
-				Main.ServerSideCharacter = true;
-				player.SendData(PacketTypes.WorldInfo);
+				if (!ssc)
+				{
+					Main.ServerSideCharacter = true;
+					player.SendData(PacketTypes.WorldInfo);
+				}
+
+				data.RestoreCharacter(player);
 			}
-
-			data.RestoreCharacter(player);
-
-			if (!ssc)
+			finally
 			{
-				Main.ServerSideCharacter = false;
-				player.SendData(PacketTypes.WorldInfo);
+				if (!ssc)
+				{
+					Main.ServerSideCharacter = false;
+					player.SendData(PacketTypes.WorldInfo);
+				}
 			}
 		}
 	}
